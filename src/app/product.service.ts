7999e877a18e81097a7652be4ad2c77f1144f635
xutpuu <xutpuu@gmail.com>
2018-09-26 17:24:31 +0300

    Completed the module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ import { Product } from './product';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  private _productsUrl ='./assets/products.json';
+  private _productsUrl ='../assets/products.json';
 
   constructor(private _http: Http) { }
 
